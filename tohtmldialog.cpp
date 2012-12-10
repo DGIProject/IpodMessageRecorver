4252b95f6341ea26ff45203009415034a853d7c9
Willena <villenagui.free.fr@gmail.com>
2012-12-10 18:39:26 +0100

    Ajout des fonction permettant l'exportation html (vers un fichier en general )

--- tohtmldialog.cpp
+++ tohtmldialog.cpp
@@ -1,6 +1,8 @@
 #include "tohtmldialog.h"
 #include "ui_tohtmldialog.h"
 #include <QtSql>
+#include <QtGui>
+#include <QFile>
 
 ToHtmlDialog::ToHtmlDialog(QWidget *parent, QSqlDatabase db) :
     QDialog(parent),
@@ -9,6 +11,35 @@ ToHtmlDialog::ToHtmlDialog(QWidget *parent, QSqlDatabase db) :
     ui->setupUi(this);
     SQliteDb = db;
     ui->radioButton->setChecked(true);
+    ui->comboBox->setEnabled(false);
+    query_sqlitedb = new QSqlQuery(SQliteDb);
+    if (SQliteDb.open())
+    {
+        query_sqlitedb->clear();
+        query_sqlitedb->prepare("SELECT * FROM madrid_chat;");
+
+        if (query_sqlitedb->exec())
+        {
+
+            while(query_sqlitedb->next())
+            {
+                QVariant test2 = query_sqlitedb->value(5);
+                ui->comboBox->addItem(test2.toString());
+            }
+
+        }
+        else
+        {
+            QMessageBox::information(this, "Erreur", "Une erreur s'est produite, le fichier est peut etre endomage \n" + query_sqlitedb->lastError().text());
+        }
+    }
+    else
+    {
+        QMessageBox::information(this, "Erreur", "Une erreur s'est produite, le fichier est peut etre endomage \n" + query_sqlitedb->lastError().text());
+    }
+
+    SQliteDb.close();
+
 }
 
 ToHtmlDialog::~ToHtmlDialog()
@@ -22,6 +53,20 @@ void ToHtmlDialog::on_Btn_cancell_clicked()
 }
 void ToHtmlDialog::on_Btn_export_clicked()
 {
+    QFile htmlFile("test.txt");
+    if (htmlFile.open(QIODevice::WriteOnly))
+    {
+            QString str ="patate !";
+            QTextStream outstream(&htmlFile);
+            outstream << str;
+    }
+    else
+    {
+        QMessageBox::critical(this, "Erreur", "Une Erreur s'est produit lors de l'exportation");
+    }
+
+    htmlFile.flush();
+    htmlFile.close();
 
 }
 void ToHtmlDialog::on_radioButton_2_clicked()
