78f0043022f93c17b8b700344605b2f38743ee10
Willena <villenagui.free.fr@gmail.com>
2012-12-10 19:37:50 +0100

    Ajout du style préparé.

--- tohtmldialog.cpp
+++ tohtmldialog.cpp
@@ -54,7 +54,9 @@ void ToHtmlDialog::on_Btn_cancell_clicked()
 }
 void ToHtmlDialog::on_Btn_export_clicked()
 {
-    QFile htmlFile("test.txt");
+    QFile htmlFile("test.html"), filetoremove("test.html");
+    filetoremove.remove();
+    filetoremove.close();
     if (htmlFile.open(QIODevice::WriteOnly))
     {
         if (ui->radioButton->isChecked())
@@ -91,7 +93,7 @@ void ToHtmlDialog::on_Btn_export_clicked()
                 if (query_sqlitedb->exec())
                 {
                     int iteratorcanevol = 0;
-                    QString FullLine = " ";
+                    QString FullLine = "<!DOCTYPE html><html><head><title>Liste des conversations</title><style>.top{background-image: url('imgs/haut-message-rec.png');width: 298px;height: 40px;margin-left: 9px;}.middle{background-image: url('imgs/haut-millieu-rec.png');margin-left: 9px;width: 298px;}.bas{background-image: url('imgs/haut-ba-rec.png');height: 40px;width: 307px;}.top-up{background-image: url('imgs/haut-message-send.png');width: 298px;height: 40px;}.bas-up{background-image: url('imgs/haut-ba-send.png');height: 40px;width: 307px;margin-left: 1px;}.midle-up{background-image: url('imgs/haut-millieu-send.png');width: 298px;}.sender{text-align: right; margin-left: 60%;position: relative;}</style></head><body>";
                     int x = 3;
                     int x2 = 31;
                     int totallines = 0;
@@ -101,7 +103,7 @@ void ToHtmlDialog::on_Btn_export_clicked()
 
                         if ( query_sqlitedb->value(x2) == "0")
                         {
-                            QString toAppend = "<p>" + query_sqlitedb->value(x).toString() + "</p>";
+                            QString toAppend = "<div><div class='top'></div><div class='middle'><div style='margin-left: 20px;margin-right:10px ;'>"  + query_sqlitedb->value(x).toString() + "</div></div><div class='bas'></div></div>";
                             qDebug() <<  "deux :" << query_sqlitedb->value(x).toString() ;
                             FullLine += toAppend;
                             iteratorcanevol++;
@@ -110,7 +112,7 @@ void ToHtmlDialog::on_Btn_export_clicked()
                         }
                         else
                         {
-                            QString ToAppend = "<p align='right'>" + query_sqlitedb->value(x).toString() + "</p>";
+                            QString ToAppend = "<div class='sender'><div class='top-up'></div><div class='midle-up'><div style='margin-right: 20px;margin-left:10px ;'>"+ query_sqlitedb->value(x).toString() + "</div></div><div class='bas-up'></div></div>";
                             qDebug() << query_sqlitedb->value(x).toString() ;
                             FullLine += ToAppend;
                             iteratorcanevol++;
